342515a2bf02ecd77d556fe5b08e39b6b135f9dc
Mundovirtual <38699384+Mundovirtual@users.noreply.github.com>
2018-10-16 13:39:58 -0500

    MasCambios

--- Administrador/class/conexion.php
+++ Administrador/class/conexion.php
@@ -1,18 +1,13 @@
 <?php 
  
 class Conectar{
-		private $servidor="localhost";
-		private $usuario="root";
-		private $bd="inovahack";
-		private $password="";
-/*
+ 
 private $servidor="localhost";
 		private $usuario="root";
 		private $bd="hacky";
 		private $password="InnovaHack18";
 
-
-*/
+ 
 		public function conexion(){
 			$conexion=mysqli_connect($this->servidor,
 									 $this->usuario,
--- principal/conexion/abrirconexion.php
+++ principal/conexion/abrirconexion.php
@@ -1,16 +1,11 @@
 <?php 
 class Conexion extends mysqli
 {
-private $HOST = "localhost";
-    private $USER = "root";
-    private $PASS = "";
-    private $BASE = "inovahack";
- 
-    /*private $HOST = "localhost";
+ private $HOST = "localhost";
     private $USER = "root";
     private $PASS = "InnovaHack18";
     private $BASE = "hacky";
-*/
+ 
     public function __construct()
     {
         parent::__construct($this->HOST, $this->USER, $this->PASS, $this->BASE);
