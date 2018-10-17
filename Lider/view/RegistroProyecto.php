d159e37298f052991cbb4356d02afde4dc675c6b
Mundovirtual <38699384+Mundovirtual@users.noreply.github.com>
2018-10-17 07:07:49 -0500

    CambiarInclude

--- Lider/view/RegistroProyecto.php
+++ Lider/view/RegistroProyecto.php
@@ -1,6 +1,6 @@
  <?php 
 include_once("../login/security.php"); 
- require_once "../ih-agrotics/class/Proyectos.php";
+ include_once "../class/Proyectos.php";
  ?>
 
 <div class="row justify-content-center" id="RegistrarProyecto">
