7c82b2325d2b0e48cc08e8851ea5e4a36e1a0288
Mundovirtual <38699384+Mundovirtual@users.noreply.github.com>
2018-10-19 16:47:33 -0500

    Hacker

--- Administrador/class/hacker.php
+++ Administrador/class/hacker.php
@@ -23,7 +23,7 @@ include_once("conexion.php");
 	 	function ActualizarDatosHacker($id,$Email,$psw,$Celular){ 
 	 		$con=new Conectar();
 	 		$Conexion=$con->conexion();
- 	 		$sql="UPDATE `comunidad` SET `E-mail`='$Email',`psw`='$psw',`Celular`='$Celular'  WHERE `id`='$id'";  
+	 		$sql="UPDATE `comunidad` SET `E-mail`='$Email',`psw`='$psw',`Celular`='$Celular'  WHERE `id`='$id'";  
 	 		$resultado=mysqli_query($Conexion,$sql); 
 	 		return $resultado;
 	 		$Conexion->mysql_close();
