f6c383dd316b9a4dae9e5209f07aa04febb10c07
Jovanny Leobardo Salas <jovannyleobardo10@gmail.com>
2018-10-25 16:06:56 -0500

    Dashboard

--- Administrador/view/index.php
+++ Administrador/view/index.php
@@ -43,6 +43,8 @@
     <!-- Data tables-->
     <link rel="stylesheet" type="text/css" href="../js/DataTables/datatables.min.css">
     <script type="text/javascript" src="../js/DataTables/datatables.js"></script>
+    <!--====================================DASBOARD======================================-->
+    <link rel="stylesheet" href="../css/dasboard.css">
   </head>
   <body>
     <!-- Side Navbar -->
@@ -163,7 +165,92 @@
                     $enrutador->cargarVista($_GET['cargar']);
                  } 
                  else{
-              
+                  ?>
+                  <div class="row">
+                      <div class="col-md">
+                        <h1 class="text-center">InnovaHack 2018</h1>
+                      </div>  
+                  </div>
+                  <div class="row">
+                      <div class="col-xl-3 col-sm-6 mb-3">
+                            <div class="propiedades card text-white" style="background-color: #588ec8">
+                              <div class="card-body">
+                                <div class="row">
+                                  <div class="col-md-3">
+                                    <i class="fas fa-tshirt fa-5x"></i>
+                                  </div>
+                                  <div class="col-md-9 text-right">
+                                    <div class="fa-3x">45</div>
+                                    <div>Playeras!</div>
+                                  </div>
+                                </div>
+                              </div>
+                              <a class="card-footer text-white clearfix small z-1 text-center" style="background-color: #588ec8">
+                                <span>CHICA</span>
+                              </a>
+                            </div>
+                      </div>
+
+                      <div class="col-xl-3 col-sm-6 mb-3">
+                            <div class="propiedades card text-white" style="background-color: #41b95d">
+                              <div class="card-body">
+                                <div class="row">
+                                  <div class="col-md-3">
+                                    <i class="fas fa-tshirt fa-5x"></i>
+                                  </div>
+                                  <div class="col-md-9 text-right">
+                                    <div class="fa-3x">124</div>
+                                    <div>Playeras!</div>
+                                  </div>
+                                </div>
+                              </div>
+                              <a class="card-footer text-white clearfix small z-2 text-center" style="background-color: #41b95d">
+                                <span>MEDIANA</span>
+                              </a>
+                            </div>
+                      </div>
+
+                      <div class="col-xl-3 col-sm-6 mb-3">
+                            <div class="propiedades card text-white" style="background-color: #eebe2f">
+                              <div class="card-body">
+                                <div class="row">
+                                  <div class="col-md-3">
+                                    <i class="fas fa-tshirt fa-5x"></i>
+                                  </div>
+                                  <div class="col-md-9 text-right">
+                                    <div class="fa-3x">30</div>
+                                    <div>Playeras!</div>
+                                  </div>
+                                </div>
+                              </div>
+                              <a class="card-footer text-white clearfix small z-1 text-center" style="background-color: #eebe2f">
+                                <span>GRANDE</span>
+                              </a>
+                            </div>
+                      </div>
+
+                      <div class="col-xl-3 col-sm-6 mb-3">
+                            <div class="propiedades card text-white bg-danger">
+                              <div class="card-body">
+                                <div class="row">
+                                  <div class="col-md-3">
+                                    <i class="fas fa-tshirt fa-5x"></i>
+                                  </div>
+                                  <div class="col-md-9 text-right">
+                                    <div class="fa-3x">27</div>
+                                    <div>Playeras!</div>
+                                  </div>
+                                </div>
+                              </div>
+                              <a class="card-footer text-white clearfix small z-1 text-center" style="background-color: #dc3545">
+                                <span>EXTRA-GRANDE</span>
+                              </a>
+                            </div>
+                      </div>
+                  </div>
+
+                  <?php
+
                  }
             ?>
            
