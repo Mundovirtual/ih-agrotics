91e3ed7807b3bb4fa13dbb75d3f90821673d2f27
Isaac Santiago Coronel <38699384+Mundovirtual@users.noreply.github.com>
2019-02-28 07:48:55 -0600

    Correcciones

--- principal/conexion/abrirconexion.php
+++ principal/conexion/abrirconexion.php
@@ -3,8 +3,8 @@ class Conexion extends mysqli
 {
  private $HOST = "localhost";
     private $USER = "root";
-    private $PASS = "InnovaHack18"; 
-    private $BASE = "innovahack"; 
+    private $PASS = ""; 
+    private $BASE = "hacky"; 
     public function __construct()
     {
         parent::__construct($this->HOST, $this->USER, $this->PASS, $this->BASE);
