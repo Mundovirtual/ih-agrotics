8be5519663261d60748ff401cf01b16bebdbb50d
Isaac Santiago Coronel <38699384+Mundovirtual@users.noreply.github.com>
2019-03-02 07:33:50 -0600

    bd change

--- principal/conexion/abrirconexion.php
+++ principal/conexion/abrirconexion.php
@@ -4,7 +4,7 @@ class Conexion extends mysqli
  private $HOST = "localhost";
     private $USER = "root";
     private $PASS = ""; 
-    private $BASE = "hackyy"; 
+    private $BASE = "hacky"; 
     public function __construct()
     {
         parent::__construct($this->HOST, $this->USER, $this->PASS, $this->BASE);
