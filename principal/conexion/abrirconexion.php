4f2f752539b5edd4994d88d0784f895579e7b6f5
Isaac Santiago Coronel <38699384+Mundovirtual@users.noreply.github.com>
2019-04-04 09:38:46 -0600

    Cmabios

--- principal/conexion/abrirconexion.php
+++ principal/conexion/abrirconexion.php
@@ -3,8 +3,8 @@ class Conexion extends mysqli
 {
  private $HOST = "localhost";
     private $USER = "root";
-    private $BASE = "innovahack";
-    private $PASS = "InnovaHack18"; 
+    private $BASE = "hack";
+    private $PASS = ""; 
      
     public function __construct()
     {
